09bdd34323ecea368f768fc2e00317d23ffd96c4
Asunaro276 <ryuhei276276@gmail.com>
2025-03-09 23:06:06 +0900

    [update] lambda上で実行できるよう修正

---
+++ cms_api/go/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+		Name string `json:"name"`
+}
+ 
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, name MyEvent) (string, error) {
+    return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+ 
